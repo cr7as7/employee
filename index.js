318c0679ceddb5e7910866b6277c7bc11036b88f
Amber Singh <ambersinghrok@gmail.com>
2021-05-19 16:24:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+
+
+form.addEventListener('submit',(event) => {
+  event.preventDefault();
+})
