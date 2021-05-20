ecd5cfbc583eb1a9a524c820d11d7c86f150e941
Amber Singh <ambersinghrok@gmail.com>
2021-05-20 13:00:50 +0530

    local storage working

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
 const spouse = document.getElementById('spouse');
 const notmarried = document.getElementById('notmarried');
+var save_value=document.getElementById("save_value");
+var read_localstorage=document.getElementById("read_localstorage");
+var localstorage_value=document.getElementById("localstorage_value");
+
+  save_value.onclick=function(){
+    localStorage.setItem(firstname.value,firstname.value);
+  }
+
+  read_localstorage.onclick=function(){
+    localstorage_value.textContent=localStorage.getItem(firstname.value);
+}
+
+
+
+
 
 function enable() {
   document.getElementById("spouse").disabled = false;
@@ -28,14 +43,26 @@ function onsubmitt() {
  
 }
 function onempty(){
-   var x=document.forms["myForm"]["firstname"].value;
+   var x=document.forms["myForm"]["firstname"].value.trim();
    if(x== ""){
-   alert("name must be filled out");
+  //  alert("name must be filled out");
+    firstname.style.border="1px solid red";
   return false; 
   }
+   x=document.forms["myForm"]["lastname"].value.trim();
+   if(x== ""){
+   alert("last name must be filled out");
+  return false; 
+  }
+  x=document.forms["myForm"]["email"].value.trim();
+   if(x== ""){
+   alert("email must be filled out");
+  return false; 
+  }
+  return true;
 }
 function validateform(){
-  return (onsubmitt()&onempty());
+  return (onsubmitt() && onempty());
 }
 
 
