bdd328236c71e27b02b0683c2b5f21c96ccf550a
Amber Singh <ambersinghrok@gmail.com>
2021-05-20 19:48:25 +0530

    email validation working

--- index.js
+++ index.js
@@ -2,28 +2,31 @@ const form = document.getElementById('form');
 const firstname = document.getElementById('firstname');
 const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
-const spouse = document.getElementById('spouse');
+let spouse = document.getElementById('spouse');
 const notmarried = document.getElementById('notmarried');
-var save_value=document.getElementById("save_value");
-var read_localstorage=document.getElementById("read_localstorage");
-var localstorage_value=document.getElementById("localstorage_value");
-var read_all_data=document.getElementById("read_all_data");
+// var save_value=document.getElementById("save_value");
+// var read_localstorage=document.getElementById("read_localstorage");
+// var localstorage_value=document.getElementById("localstorage_value");
+// var read_all_data=document.getElementById("read_all_data");
+if(spouse.value ==="")
+spouse.value="N/A";
+let storearray=[];
+function display(){
+    localStorage.setItem("added-items",firstname.value+" "+lastname.value+" "+email.value());
+   
+  for (var i = 0; i < localStorage.length; i++){
+     let x=localStorage.getItem(localStorage.key(i));
+     storearray.push(x);
+}
+console.log(storearray);
+    document.getElementById("display_local_storage_data").innerHTML=storearray;
 
-  save_value.onclick=function(){
-    localStorage.setItem(firstname.value,firstname.value);
-  }
-
-  read_localstorage.onclick=function(){
-    localstorage_value.textContent=localStorage.getItem(firstname.value);
 }
 
-read_all_data.onclick=function(){
-  var keys=Object.keys(localStorage);
-  console.log(keys);
-  for(var key of keys){
-    console.log("Key : "+key+" : Value : "+localStorage.getItem(key));
-}
-}
+
+
+
+
 document.querySelectorAll('input').forEach((input) => {
   input.addEventListener('blur', function() {
     this.classList.toggle('green', this.value.length > 0);
@@ -38,6 +41,7 @@ function disable() {
   document.getElementById("spouse").value = "";
   document.getElementById("spouse").disabled = true;
 }
+
 function onsubmitt() {
   // const firstnamval = firstname.value.trim();
   // const lastnamval = lastname.value.trim();
@@ -53,22 +57,44 @@ function onsubmitt() {
   }
  
 }
+function isEmail(emailval){
+  var atSymbol = emailval.indexOf("@");
+  if(atSymbol < 1)
+  return false;
+  var dot =emailval.lastIndexOf('.');
+  if(dot <=atSymbol+2)
+  return false;
+  if(dot === emailval.length - 1)
+  return false;
+
+  return true;
+
+}
 function onempty(){
    var x=document.forms["myForm"]["firstname"].value.trim();
    if(x== ""){
   //  alert("name must be filled out");
     firstname.style.border="1px solid red";
   return false; 
+  }else
+  {
+    firstname.style.border="1px solid #2ecc71";
   }
    x=document.forms["myForm"]["lastname"].value.trim();
    if(x== ""){
     lastname.style.border="1px solid red";
   return false; 
+  }else
+  {
+    lastname.style.border="1px solid #2ecc71";
   }
   x=document.forms["myForm"]["email"].value.trim();
-   if(x== ""){
+   if(!isEmail(x)){
     email.style.border="1px solid red";
   return false; 
+  }else
+  {
+    email.style.border="1px solid #2ecc71";
   }
   return true;
 }
